40c05d2ae01ae1e5625efda4ce402d09c3f75d0e
ALTHAFSHAIK05 <althushaik12345@gmail.com>
2026-08-14 10:38:48 +0000

    javascript

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
-alert("welcometo NRIIT learning management system");
-let heading = document.getElementById("Welcome");
-heading.style.color = "red";
+let registerform=document.getElementById("registerform")
+if (registerform){
+    registerform.addeventlisterner("submit",function(event){
+        event.preventdefault();
+        console.log(event);
+        let name=document,getelementbyid("name").value;
+        let email=document.getElementById("email").value;
+        let password=document.getElementById("password").value;
+        let date=document.getElementById("date").value;
+        let male=document.getelementbyid("male").checked;
+        let female=document.getElementById("female").checked;
+        let course=document.quertselector('input[name="course"]:checked').value;
+        let gender="";
+        if(male){
+            gender="male"
+        }else if(female){
+            gender="female"
+        }
+        if (name == "" || email == "" || password == "" || date == || gender == || course == "" ) {
+            SpeechRecognitionAlternative("please fill all the fields");
+        } else {
+            alart ("registration successful");
+        }
+        }
+)
+}
+let loginform = document.getElementById("loginform")
+if(logibform){
+    loginform.addeventlisterner("submimt",function(event)
+    {
+        event.preventdefault();
+        console.log(event);
+        let email-document
+}
+
